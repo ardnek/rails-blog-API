b7e894ec971f277abab0196296dc1260f9fa76c3
Kendra <kdodds@gmail.com>
2015-02-11 13:53:41 -0700

    working through associations posts, users, comments

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :id, :title, :body
+  attributes :id, :user_id, :title, :body
+
+  has_one :user, except: :posts
+  has_many :comments, except: :posts
 end
